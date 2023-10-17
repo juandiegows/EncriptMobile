4271dfa2f534d89826ae63d04615f7099a512ee6
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2023-10-16 22:05:01 -0500

    encriptar con varios algoritmos

--- app/src/main/java/com/example/encriptmobile/MainActivity.kt
+++ app/src/main/java/com/example/encriptmobile/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.encriptmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.core.widget.addTextChangedListener
 import com.example.encriptmobile.databinding.ActivityMainBinding
 import java.security.MessageDigest
@@ -10,17 +11,48 @@ import java.security.spec.MGF1ParameterSpec
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    var algoritmos = listOf(
+    "MD5",
+    "SHA1",
+    "SHA256",
+    "SHA512",
+    "AES",
+    "DES",
+    "RSA",
+    "DSA",
+    "Blowfish",
+    "Triple DES",
+    "RC4",
+    "HMAC",
+    "ECC",
+    "Diffie-Hellman",
+    "PGP",
+    "GPG",
+    "SSL/TLS",
+    "Bcrypt",
+    "Argon2",
+    "Scrypt"
+    )
 
     //fun ByteArray.toHex() = joinToString(separator = "") { byte -> "%02x".format(byte) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.spinner.adapter =  ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item, algoritmos)
         binding.txtText.addTextChangedListener {
-            EncriptSHA1()
+            Encript(binding.spinner.selectedItem.toString())
         }
     }
-
+    private fun Encript(algorithm : String) {
+        MessageDigest.getInstance(algorithm).apply {
+            var text =
+                this.digest(binding.txtText.text.toString().encodeToByteArray()).joinToString("") {
+                    "%02x".format(it)
+                }
+            binding.txtEncxript.text = text
+        }
+    }
     private fun EncriptSHA1() {
         MessageDigest.getInstance("SHA1").apply {
             var text =
