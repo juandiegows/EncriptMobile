c6f319b5b1ceea94739d8e6656a015a3422e9a79
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2023-10-16 22:14:07 -0500

    evitar errores y cambiar el texto al selecionar el spinner

--- app/src/main/java/com/example/encriptmobile/MainActivity.kt
+++ app/src/main/java/com/example/encriptmobile/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.encriptmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.core.widget.addTextChangedListener
 import com.example.encriptmobile.databinding.ActivityMainBinding
+import java.lang.Exception
 import java.security.MessageDigest
 import java.security.spec.MGF1ParameterSpec
 
@@ -43,15 +46,29 @@ class MainActivity : AppCompatActivity() {
         binding.txtText.addTextChangedListener {
             Encript(binding.spinner.selectedItem.toString())
         }
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                Encript(binding.spinner.selectedItem.toString())
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                Encript(binding.spinner.selectedItem.toString())
+            }
+        }
     }
     private fun Encript(algorithm : String) {
-        MessageDigest.getInstance(algorithm).apply {
-            var text =
-                this.digest(binding.txtText.text.toString().encodeToByteArray()).joinToString("") {
-                    "%02x".format(it)
-                }
-            binding.txtEncxript.text = text
+        try {
+            MessageDigest.getInstance(algorithm).apply {
+                var text =
+                    this.digest(binding.txtText.text.toString().encodeToByteArray()).joinToString("") {
+                        "%02x".format(it)
+                    }
+                binding.txtEncxript.text = text
+            }
+        }catch (e: Exception){
+            binding.txtEncxript.text = "No se puede con ${binding.spinner.selectedItem.toString()} :("
         }
+
     }
     private fun EncriptSHA1() {
         MessageDigest.getInstance("SHA1").apply {
